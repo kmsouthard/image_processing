337df7cb5c10b662f77f493c4b0fbe8502cccf12
kmsouthard <kade.southard@gmail.com>
2015-08-18 12:46:20 -0700

    started coding segmentation, code for processing each file

--- backgroundSubtraction.py
+++ backgroundSubtraction.py
@@ -18,65 +18,76 @@ import matplotlib.pyplot as plt
 from scipy.ndimage import median_filter 
 from skimage import io, exposure, img_as_uint, img_as_float
 from skimage.morphology import reconstruction
+from skimage.filters import threshold_otsu
 
 import skimage.io as io
 #io.use_plugin('tifffile')
+
+#fn ="Snap-2066-Image Export-01_c1_ORG.tif"
+#radius = 2
+#height = 0.8
 
 def background_subtraction(filename, radius, height):
     #load the image
     image = img_as_float(io.imread(filename))
     #apply median filter with given radius
-    image = median_filter(image, 2)
+    image = median_filter(image, radius)
     seed = np.copy(image) - height
     mask = image
     dilated = reconstruction(seed, mask, method='dilation')
     hdome = image - dilated
+    
+    return {'hdome': hdome, 'mask': mask, 'dilated': dilated, 'seed': seed}    
 
-    #background subtracted image is now known as hdome
-
+def plot_summary(im_dict, filename, path):
     #line plot through image, dilated image, background subtracted image
     fig, (ax1, ax2, ax3) = plt.subplots(ncols=3, figsize=(10, 5))
 
     yslice = 197
 
-    ax1.plot(mask[yslice], '0.5', label='mask')
-    ax1.plot(seed[yslice], 'k', label='seed')
-    ax1.plot(dilated[yslice], 'r', label='dilated')
+    ax1.plot(im_dict['mask'][yslice], '0.5', label='mask')
+    ax1.plot(im_dict['seed'][yslice], 'k', label='seed')
+    ax1.plot(im_dict['dilated'][yslice], 'r', label='dilated')
     ax1.set_ylim(-0.2, 1.2)
     ax1.set_title('image slice')
     ax1.set_xticks([])
     ax1.legend()
 
-    ax2.imshow(dilated, vmin=image.min(), vmax=image.max())
+    ax2.imshow(im_dict['dilated'], vmin=im_dict['mask'].min(), vmax=im_dict['mask'].max())
     ax2.axhline(yslice, color='r', alpha=0.4)
     ax2.set_title('dilated')
     ax2.axis('off')
-
-    ax3.imshow(hdome)
+    
+    ax3.imshow(im_dict['hdome'])
     ax3.axhline(yslice, color='r', alpha=0.4)
     ax3.set_title('image - dilated')
     ax3.axis('off')
-
+    
     fig.tight_layout()
     #save summary image
-    plt.savefig('foo.png', format='png')
+    plt.savefig(path+'summary'+filename[:-4]+'.png', format='png')
+    plt.close()
+    return
 
-    fig, ax = plt.subplots(figsize=(10, 10))
-    ax.imshow(hdome, cmap=plt.cm.gray)
-    ax.axis('off')
-    ax.set_title('paxcillin-mcherry')
-    plt.savefig('bar.png')
-
-    plt.close('all')
-
+def save_output(im_dict, filename, path):
     io.use_plugin('freeimage')
-    im = hdome
+    im = im_dict['hdome']
     im = exposure.rescale_intensity(im, out_range='float')
     im = img_as_uint(im)
-
-    io.imsave('test_16bit.tif', im)
+    io.imsave(path+'output'+filename[:-4]+'.png', im)
     return
-   
+    
+def segmentation(im_dict, filename, path):
+    io.use_plugin('freeimage')
+    thresh = threshold_otsu(im_dict['hdome'])
+    binary = im_dict['hdome'] > thresh
+    io.imsave(path+'binary'+filename[:-4]+'.png', binary)
+    return
+
+#bs=  background_subtraction(fn, radius, height)
+#plot_summary(bs, fn)
+#save_output(bs, fn)
+  
 #filename ="Snap-2066-Image Export-01_c1_ORG.tif"
 #radius = 2
 #height = 0.90
--- processEachFile.py
+++ processEachFile.py
@@ -1,18 +1,26 @@
 import os
 import fnmatch
 from backgroundSubtraction import background_subtraction
+from backgroundSubtraction import plot_summary
+from backgroundSubtraction import save_output
+from backgroundSubtraction import segmentation
 
 path = '/Users/southk/dataAnalysis/Focal Adhesion Analysis/2014.6 pax with all processed/RAW'
 
 #process channel 1 (paxcillin)
-i = 0
+
 pattern = '*c1_ORG.tif'
 radius = 2
-height = 0.9
+height = 0.80
+i = 0
 for (path, dirs, files) in os.walk(path):
     for filename in fnmatch.filter(files, pattern):
         print filename
-        background_subtraction(os.path.join(path, filename), radius, height)
+        backSub = background_subtraction(os.path.join(path, filename), radius, height)
+        plot_summary(backSub, filename, path)
+        save_output(backSub, filename, path)
+        segmentation(backSub, filename, path)
         i += 1
-    if i > 4:
-        break
+        if i > 10:
+            break
+        
